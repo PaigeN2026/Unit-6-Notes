a7e64d40e2fac9fc651fb2c329b04c7c8466f2f8
PaigeN2026 <Paige.nunez2026@gbwl.org>
2025-01-21 13:49:53 +0000

    parallel arrays

--- Main.java
+++ Main.java
@@ -25,16 +25,31 @@ public class Main {
       // 2. Use an INITIALIZER LIST to set array values 
       int[] luckiestNumbers = { 13, 13, 10, 9, 5, 20, 37, 7 };
       double[] currentCash = { 0.0, 5.25, 1.0, 15.0, 21.0 };
-      String[] bestClass = { "Maia", "Alex", "Zoie", "Paige", "Natalie", "Bryce", "Finny", "Jackson" };
-
       // HOW TO ACCESS ARRAY ELEMENTS (items)
       // arrayNAME[index] --> gets item at that position
       System.out.println(luckiestNumbers[0]); // first item is at index 0
-      System.out.println(bestClass[5]);
 
       // HOW TO MODIFY ARRAY ELEMENTS  
       // arrayName[index] = newValue
       faveAnimals[0] = "dolphin";
       faveAnimals[1] = "dog";
+      // NOTE that the other 6 indices hold NULL
+      System.out.println(faveAnimals[2]);
+
+      String[] bestClass = { "Maia", "Alex", "Zoie", "Paige", "Natalie", "Bryce", "Finny", "Jackson" };
+      System.out.println(bestClass[5]);
+      // LENGTH is an Array ATTRIBUTE/PROPERTY 
+      int numStudents = bestClass.length;
+      System.out.println(numStudents);
+      // FINAL INDEX is always [length - 1] !!!
+      int lastStudentIndex = bestClass.length - 1; // 7
+      System.out.println(bestClass[lastStudentIndex]);
+
+      // PARALLEL ARRAY to hold info associated with another arrat
+      String[] faveFoods = { "Pasta", "", "Sushi", "Caesar Salad", "Sushi", "Burritos", "I don't know", "Lasagna" };
+      // With parallel arrays, ORDER MATTERS
+      // 
+      System.out.println(bestClass[4] + "'s favorite food is " + faveFoods[4]);
+      System.out.println(bestClass[3] + "'s favorite food is " + faveFoods[3]);
    }
 }
